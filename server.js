27c777eb969758f264ff3b0884f901b58e3a3a98
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-03-05 00:50:05 -0800

    updated content security policy header in server.js and updated routes in vercel.json

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},
-    () => { console.log('connected to mongo: ', process.env.MONGO_URI)}    
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true },
+    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
 )
 
 // MIDDLEWARE
@@ -16,11 +16,15 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 // ROUTES
 app.get('/', (req, res) => {
+    res.setHeader(
+        'Content-Security-Policy-Report-Only',
+        "default-src 'self'; font-src 'self'; img-src 'self' https://images.unsplash.com; script-src 'self'; style-src 'self'; frame-src 'self';"
+    );
     res.send('Welcome to an Awesome App about Breads!')
 })
 
